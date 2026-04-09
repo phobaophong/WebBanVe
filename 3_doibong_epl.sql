48d04996e7d23218f9973f9bac452421ece06937
phobaophong <phong_dth235731@student.agu.edu.vn>
2026-04-09 08:46:01 +0700

    capnhat doi bong

--- 3_doibong_epl.sql
+++ 3_doibong_epl.sql
@@ -1,21 +1,25 @@
+--Chèn 23 đội bóng theo thứ tự chữ cái (20 EPL + 3 đội từ ảnh của bạn)
 INSERT INTO tbl_doibong (ten_doi, quoc_gia) VALUES 
-('Arsenal', 'Anh'),
-('Aston Villa', 'Anh'),
-('Bournemouth', 'Anh'),
-('Brentford', 'Anh'),
-('Brighton & Hove Albion', 'Anh'),
-('Chelsea', 'Anh'),
-('Crystal Palace', 'Anh'),
-('Everton', 'Anh'),
-('Fulham', 'Anh'),
-('Ipswich Town', 'Anh'),
-('Leicester City', 'Anh'),
-('Liverpool', 'Anh'),
-('Manchester City', 'Anh'),
-('Manchester United', 'Anh'),
-('Newcastle United', 'Anh'),
-('Nottingham Forest', 'Anh'),
-('Southampton', 'Anh'),
-('Tottenham Hotspur', 'Anh'),
-('West Ham United', 'Anh'),
-('Wolverhampton Wanderers', 'Anh');
+('Arsenal', 'Anh'),                -- ID 1
+('Aston Villa', 'Anh'),            -- ID 2
+('Bournemouth', 'Anh'),            -- ID 3
+('Brentford', 'Anh'),              -- ID 4
+('Brighton & Hove Albion', 'Anh'), -- ID 5
+('Burnley', 'Anh'),                -- ID 6
+('Chelsea', 'Anh'),                -- ID 7
+('Crystal Palace', 'Anh'),         -- ID 8
+('Everton', 'Anh'),                -- ID 9
+('Fulham', 'Anh'),                 -- ID 10
+('Ipswich Town', 'Anh'),           -- ID 11
+('Leeds', 'Anh'),                  -- ID 12
+('Leicester City', 'Anh'),         -- ID 13
+('Liverpool', 'Anh'),              -- ID 14
+('Manchester City', 'Anh'),        -- ID 15
+('Manchester United', 'Anh'),      -- ID 16
+('Newcastle United', 'Anh'),       -- ID 17
+('Nottingham Forest', 'Anh'),      -- ID 18
+('Southampton', 'Anh'),            -- ID 19
+('Sunderland', 'Anh'),             -- ID 20
+('Tottenham Hotspur', 'Anh'),      -- ID 21
+('West Ham United', 'Anh'),        -- ID 22
+('Wolverhampton Wanderers', 'Anh');-- ID 23
