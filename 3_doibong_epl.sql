1c1537f62cad3a98487bcfa258c8b4c2f4651732
phobaophong <phong_dth235731@student.agu.edu.vn>
2026-04-09 09:12:21 +0700

    update doi bong

--- 3_doibong_epl.sql
+++ 3_doibong_epl.sql
@@ -1,25 +1,21 @@
---Chèn 23 đội bóng theo thứ tự chữ cái (20 EPL + 3 đội từ ảnh của bạn)
 INSERT INTO tbl_doibong (ten_doi, quoc_gia) VALUES 
-('Arsenal', 'Anh'),                -- ID 1
-('Aston Villa', 'Anh'),            -- ID 2
-('Bournemouth', 'Anh'),            -- ID 3
-('Brentford', 'Anh'),              -- ID 4
-('Brighton & Hove Albion', 'Anh'), -- ID 5
-('Burnley', 'Anh'),                -- ID 6
-('Chelsea', 'Anh'),                -- ID 7
-('Crystal Palace', 'Anh'),         -- ID 8
-('Everton', 'Anh'),                -- ID 9
-('Fulham', 'Anh'),                 -- ID 10
-('Ipswich Town', 'Anh'),           -- ID 11
-('Leeds', 'Anh'),                  -- ID 12
-('Leicester City', 'Anh'),         -- ID 13
-('Liverpool', 'Anh'),              -- ID 14
-('Manchester City', 'Anh'),        -- ID 15
-('Manchester United', 'Anh'),      -- ID 16
-('Newcastle United', 'Anh'),       -- ID 17
-('Nottingham Forest', 'Anh'),      -- ID 18
-('Southampton', 'Anh'),            -- ID 19
-('Sunderland', 'Anh'),             -- ID 20
-('Tottenham Hotspur', 'Anh'),      -- ID 21
-('West Ham United', 'Anh'),        -- ID 22
-('Wolverhampton Wanderers', 'Anh');-- ID 23
+('Arsenal', 'Anh'),                   -- ID 1
+('Aston Villa', 'Anh'),               -- ID 2
+('Bournemouth', 'Anh'),               -- ID 3
+('Brentford', 'Anh'),                 -- ID 4
+('Brighton', 'Anh'),                  -- ID 5
+('Burnley', 'Anh'),                   -- ID 6
+('Chelsea', 'Anh'),                   -- ID 7
+('Crystal Palace', 'Anh'),            -- ID 8
+('Everton', 'Anh'),                   -- ID 9
+('Fulham', 'Anh'),                    -- ID 10
+('Leeds United', 'Anh'),              -- ID 11
+('Liverpool', 'Anh'),                 -- ID 12
+('Manchester City', 'Anh'),           -- ID 13
+('Manchester United', 'Anh'),         -- ID 14
+('Newcastle', 'Anh'),                 -- ID 15
+('Nottingham Forest', 'Anh'),         -- ID 16
+('Sunderland', 'Anh'),                -- ID 17
+('Tottenham', 'Anh'),                 -- ID 18
+('West Ham', 'Anh'),                  -- ID 19
+('Wolverhampton Wanderers', 'Anh');   -- ID 20
